d1da4a2779f627060551aed12c3478344e9fe698
nimishbongale <43414361+nimishbongale@users.noreply.github.com>
2018-12-09 20:02:10 +0530

    Update and rename Q12.java to Q12a.java

--- Q12a.java
+++ Q12a.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Q12{
+public class Q12a{
 
     public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);

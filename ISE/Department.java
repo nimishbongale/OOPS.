766b09e3b1882707bf3e3e8292858912e82cde58
nimishbongale <43414361+nimishbongale@users.noreply.github.com>
2018-12-09 15:08:06 +0530

    Add files via upload

---
+++ ISE/Department.java
@@ -0,0 +1,8 @@
+package ISE;
+public interface Department
+{
+    void readdata();
+    void printdata();
+    void print_number_of_designations();
+    void number_of_research_consultancy_projs();
+}

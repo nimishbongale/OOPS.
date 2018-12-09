914d7f10554e3f977ddc690581d985e7eb02244a
nimishbongale <43414361+nimishbongale@users.noreply.github.com>
2018-12-09 14:20:44 +0530

    Add files via upload

---
+++ Faculty.java
@@ -0,0 +1,13 @@
+package ISE;
+public class Faculty 
+{
+    static String name,desig,sub;
+    static int age,yoe,date;
+    Faculty()
+    {
+        name=desig="";
+        age=yoe=date=0;
+    }
+    
+}
+

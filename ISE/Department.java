3c7044f260f8beb1579c8a5c770383862ad66f49
nimishbongale <43414361+nimishbongale@users.noreply.github.com>
2018-12-09 15:09:06 +0530

    Delete Department.java

--- ISE/Department.java
+++
@@ -1,8 +0,0 @@
-package ISE;
-public interface Department
-{
-    void readdata();
-    void printdata();
-    void print_number_of_designations();
-    void number_of_research_consultancy_projs();
-}

41d311bb5abf97e001405fb297ee71bb6dcb543b
nimishbongale <43414361+nimishbongale@users.noreply.github.com>
2018-12-09 21:21:54 +0530

    Add files via upload

---
+++ Q12aLL.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+class Q12aLL
+
+{
+
+	public static void main(String args[])
+
+	{
+
+		Scanner sc = new Scanner(System.in);
+
+		int ch;
+
+		String s;
+
+		LinkedList<String> ll = new LinkedList<String>();
+
+		while(true){
+
+		System.out.println("1.add name to linked list");
+
+		System.out.println("2.Delete from head");
+
+		System.out.println("3.Diplay using iterator");
+
+		System.out.println("Enter choice");
+
+		ch = sc.nextInt();
+
+		switch(ch)
+
+		{
+
+			case 1:
+
+			 System.out.println("Enter name");
+
+			 s = sc.next();
+
+			 ll.add(s);
+
+			 break;
+
+			case 2:
+
+			 System.out.println("Removed name is :"+ ll.remove(0));
+
+			 break;
+
+			case 3:
+
+			if(ll.peekFirst()==null)
+
+			{
+
+				System.out.println("LL empty");
+
+			}
+
+			else
+
+			{
+
+			 Iterator<String> itr = ll.iterator();
+
+			 while(itr.hasNext())
+
+			 {
+
+			    String el = itr.next();
+
+			    if(el.length()<5)
+
+				System.out.println(el);
+
+			 }
+
+			}
+
+				break;
+
+			default:
+			 System.exit(0);
+
+		}
+
+	}
+
+ }
+
+}
---
+++ Q12bAL.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+class Q12bAL
+
+{
+
+    public static void main(String args[])
+
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        int ch;
+
+        String s;
+
+        ArrayList<String> al = new ArrayList<String>();
+
+        while(true){
+
+        System.out.println("1.add name to array list");
+
+        System.out.println("2.Delete from head");
+
+        System.out.println("3.Diplay using iterator");
+
+        System.out.println("Enter choice");
+
+        ch = sc.nextInt();
+
+        switch(ch)
+
+        {
+
+            case 1:
+
+             System.out.println("Enter name");
+
+             s = sc.next();
+
+             al.add(s);
+
+             break;
+
+            case 2:
+
+             System.out.println("Removed name is :"+ al.remove(0));
+
+             break;
+
+            case 3:
+
+             Iterator<String> itr = al.iterator();
+
+             while(itr.hasNext())
+
+             {
+
+                String el = itr.next();
+
+                if(el.length()<5)
+
+                System.out.println(el);
+
+            }
+
+                break;
+
+            default:
+             System.exit(0);
+
+        }
+
+    }
+
+ }
+
+}
